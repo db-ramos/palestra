0265c5d945f1836a1666982b0beee082e519a8be
David Ramos <davidness@gmail.com>
2023-06-16 21:37:49 -0400

    listar agora exibe uma tabela

--- listar.php
+++ listar.php
@@ -1,4 +1,4 @@
-
+    
 
 
 <div class="center">
@@ -8,9 +8,12 @@ if(empty($listaPalestra)){
     echo "<h2>Não existem palestras cadastradas. Cadastre uma palestra.</h2>";
 }else{
     echo "<h2>Existem palestras cadastradas</h2>";
+    echo "<table class='center'>";
+    echo "<tr><th>Título</th> <th>Palestrante</th> <th> Data</th> <th> Hora</th></tr>";
     foreach($listaPalestra as $p){
-        echo "<p>Palestra: $p->titulo, $p->palestrante, $p->data, $p->hora</p>";  
+        echo "<tr><td>$p->titulo</td> <td>$p->palestrante</td> <td> $p->data</td> <td> $p->hora</td></tr>";  
     }
+    echo "</table>";
 }
 
 ?>
